bb1119913a82de5286b34b8cae23ca5130d2751c
tomasmark79 <tomas@digitalspace.name>
2024-10-06 21:16:19 +0200

    Fixed look for correct Bitwig Window

--- bitwig_transport.sh
+++ bitwig_transport.sh
@@ -7,8 +7,14 @@ WINDOW_ID=$(xdotool search --name "Bitwig Studio")
 
 # If Bitwig Studio
 if [ ! -z "$WINDOW_ID" ]; then
-  # Activate Bitwig Studio
-  xdotool windowactivate --sync $WINDOW_ID
-  # Send space key to Bitwig Studio
-  xdotool key --window $WINDOW_ID space
+  # Get the window name
+  WINDOW_NAME=$(xdotool getwindowname $WINDOW_ID)
+
+  # Check if the window name contains "Bitwig Studio"
+  if [[ "$WINDOW_NAME" == *"Bitwig Studio"* ]]; then
+    # Activate Bitwig Studio
+    xdotool windowactivate --sync $WINDOW_ID
+    # Send space key to Bitwig Studio
+    xdotool key --window $WINDOW_ID space
+  fi
 fi
