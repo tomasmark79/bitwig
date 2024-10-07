48e6fccdfdb764547a5427a24f2afeeb3802ee80
tomasmark79 <tomas@digitalspace.name>
2024-10-08 00:39:51 +0200

    relative paths

--- start_environment.sh
+++ start_environment.sh
@@ -5,17 +5,9 @@
 # Notify
 notify-send "Starting transport control environment..."
 
-# Check if the source file exists before creating a symbolic link
-if [ -f ~/dev/bash/bitwig/.xbindkeysrc_bitwig ]; then
-    ln -sf ~/dev/bash/bitwig/.xbindkeysrc_bitwig ~/.xbindkeysrc_bitwig
-else
-    echo "Source file ~/dev/bash/bitwig/.xbindkeysrc_bitwig does not exist."
-    exit 1
-fi
-
 # Kill all xbindkeys and xbindkeys-show processes if they are running
 pkill -x xbindkeys 2>/dev/null
 pkill -x xbindkeys-show 2>/dev/null
 
 # Start xbindkeys with the specified configuration file
-xbindkeys -f ~/.xbindkeysrc_bitwig
+xbindkeys -f .xbindkeysrc_bitwig
